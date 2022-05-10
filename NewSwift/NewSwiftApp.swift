99434f218c34439a0f2d22cf8345631f04cf7c9a
Sergey <kopgamerk@gmail.com>
2022-05-10 19:00:39 +0300

    Initial Commit

---
+++ NewSwift/NewSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewSwiftApp.swift
+//  NewSwift
+//
+//  Created by Сергей Веретенников on 10/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct NewSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
